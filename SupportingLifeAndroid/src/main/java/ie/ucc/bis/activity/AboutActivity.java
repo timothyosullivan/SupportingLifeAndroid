528fb7bd363f17c408a4d4f73a3e1f5061ed1fe0
TOSullivan <TOSullivan@BIS370a.afis.ad.ucc.ie>
2013-10-31 17:56:34 +0000

    tosulliv: initial work on about screen

--- SupportingLifeAndroid/src/main/java/ie/ucc/bis/activity/AboutActivity.java
+++ SupportingLifeAndroid/src/main/java/ie/ucc/bis/activity/AboutActivity.java
@@ -1,12 +1,7 @@
 package ie.ucc.bis.activity;
 
 import ie.ucc.bis.R;
-import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 
 /**
@@ -20,8 +15,6 @@ import android.widget.TextView;
  *
  */
 public class AboutActivity extends SupportingLifeBaseActivity {
-
-	private static final float SUPPORTING_LIFE_TEXT_SIZE = 12;
 
 	/**
 	 * onCreate method
@@ -37,14 +30,9 @@ public class AboutActivity extends SupportingLifeBaseActivity {
 
 		setContentView(R.layout.activity_about);		
 		setTitleFromActivityLabel(R.id.action_bar_title_text);
-		
-		// configure a scroll bar on the about screen
-		final TextView supportingLifeTextView = (TextView)findViewById(R.id.about_text_content);
-		supportingLifeTextView.setTextSize(SUPPORTING_LIFE_TEXT_SIZE);
-		supportingLifeTextView.setMovementMethod(new ScrollingMovementMethod());
 
 		// configure animation of Supporting LIFE images
-		configureAndRunImageAnimator();
+		/* configureAndRunImageAnimator(); */
 	}
 
 	/**
@@ -56,7 +44,7 @@ public class AboutActivity extends SupportingLifeBaseActivity {
 	 * Image animation is handled in a separate thread.
 	 * 
 	 */	
-	private void configureAndRunImageAnimator() {
+/*	private void configureAndRunImageAnimator() {
 		// Load the ImageView that will host the supporting life 
 		// image animation and set its background to our 
 		// AnimationDrawable XML resource.
@@ -75,5 +63,5 @@ public class AboutActivity extends SupportingLifeBaseActivity {
 			} // end of run() method
 		});	// end of Runnable inner class
 	}
-
+*/
 }
