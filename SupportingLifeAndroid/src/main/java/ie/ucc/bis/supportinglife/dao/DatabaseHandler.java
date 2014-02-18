9c84101a8484fb65c3f9f1f859e5c02fbdd5abc4
TOSullivan <TOSullivan@BIS370a.afis.ad.ucc.ie>
2014-02-18 10:40:48 +0000

    tosulliv: addition of database handling on Android device

---
+++ SupportingLifeAndroid/src/main/java/ie/ucc/bis/supportinglife/dao/DatabaseHandler.java
@@ -0,0 +1,143 @@
+package ie.ucc.bis.supportinglife.dao;
+
+import ie.ucc.bis.supportinglife.assessment.model.listener.DateDialogSetListener;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+	
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "supportinglife.db";
+
+	// Tables
+	protected static final String TABLE_PATIENT = "patient";
+	
+	// 'Patient' Table Columns
+	protected static final String TABLE_PATIENT_COLUMN_ID = "_id";
+	protected static final String TABLE_PATIENT_COLUMN_NATIONAL_ID = "national_id";
+	protected static final String TABLE_PATIENT_COLUMN_NATIONAL_HEALTH_ID = "national_health_id";
+	protected static final String TABLE_PATIENT_COLUMN_HSA_USER_ID = "hsa_user_id";
+	protected static final String TABLE_PATIENT_COLUMN_CHILD_FIRST_NAME = "child_first_name";
+	protected static final String TABLE_PATIENT_COLUMN_CHILD_SURNAME = "child_surname";
+	protected static final String TABLE_PATIENT_COLUMN_BIRTH_DATE = "birth_date";
+	protected static final String TABLE_PATIENT_COLUMN_GENDER = "gender";
+	protected static final String TABLE_PATIENT_COLUMN_CAREGIVER_NAME = "caregiver";
+	protected static final String TABLE_PATIENT_COLUMN_RELATIONSHIP = "relationship";
+	protected static final String TABLE_PATIENT_COLUMN_PHYSICAL_ADDRESS = "physical_address";
+	protected static final String TABLE_PATIENT_COLUMN_VILLAGE_TA = "village_ta";
+	protected static final String TABLE_PATIENT_COLUMN_VISIT_DATE = "visit_date";
+	protected static final String TABLE_PATIENT_COLUMN_CHEST_INDRAWING = "chest_indrawing";
+	protected static final String TABLE_PATIENT_COLUMN_BREATHS_PER_MINUTE = "breaths_per_minute";
+	protected static final String TABLE_PATIENT_COLUMN_SLEEPY_UNCONSCIOUS = "sleepy_unconscious";
+	protected static final String TABLE_PATIENT_COLUMN_PALMAR_PALLOR = "palmar_pallor";
+	protected static final String TABLE_PATIENT_COLUMN_MUAC_TAPE_COLOUR = "muac_tape_colour";
+	protected static final String TABLE_PATIENT_COLUMN_SWELLING_BOTH_FEET = "swelling_both_feet";
+	protected static final String TABLE_PATIENT_COLUMN_PROBLEM = "problem";
+	protected static final String TABLE_PATIENT_COLUMN_COUGH = "cough";
+	protected static final String TABLE_PATIENT_COLUMN_COUGH_DURATION = "cough_duration";
+	protected static final String TABLE_PATIENT_COLUMN_DIARRHOEA = "diarrhoea";
+	protected static final String TABLE_PATIENT_COLUMN_DIARRHOEA_DURATION = "diarrhoea_duration";
+	protected static final String TABLE_PATIENT_COLUMN_BLOOD_IN_STOOL = "blood_in_stool";
+	protected static final String TABLE_PATIENT_COLUMN_FEVER= "fever";
+	protected static final String TABLE_PATIENT_COLUMN_FEVER_DURATION = "fever_duration";
+	protected static final String TABLE_PATIENT_COLUMN_CONVULSIONS = "convulsions";
+	protected static final String TABLE_PATIENT_COLUMN_DIFFICULTY_DRINKING= "difficulty_drinking";
+	protected static final String TABLE_PATIENT_COLUMN_UNABLE_TO_DRINK = "unable_to_drink";
+	protected static final String TABLE_PATIENT_COLUMN_VOMITING = "vomiting";	
+	protected static final String TABLE_PATIENT_COLUMN_VOMITS_EVERYTHING= "vomits_everything";
+	protected static final String TABLE_PATIENT_COLUMN_RED_EYE = "red_eye";
+	protected static final String TABLE_PATIENT_COLUMN_RED_EYE_DURATION = "red_eye_duration";	
+	protected static final String TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING= "difficulty_seeing";
+	protected static final String TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING_DURATION = "difficulty_seeing_duration";
+	protected static final String TABLE_PATIENT_COLUMN_CANNOT_TREAT = "cannot_treat";		
+	protected static final String TABLE_PATIENT_COLUMN_CANNOT_TREAT_DETAILS = "cannot_treat_details";		
+	
+	// Database creation SQL statement
+	private static final String DATABASE_CREATE = "CREATE TABLE "
+			+ TABLE_PATIENT + "(" 	+ TABLE_PATIENT_COLUMN_ID							+ " INTEGER PRIMARY KEY AUTOINCREMENT, " 
+									+ TABLE_PATIENT_COLUMN_NATIONAL_ID 					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_NATIONAL_HEALTH_ID 			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_HSA_USER_ID 					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_CHILD_FIRST_NAME 			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_CHILD_SURNAME 				+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_BIRTH_DATE 					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_GENDER 						+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_CAREGIVER_NAME 				+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_RELATIONSHIP 				+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_PHYSICAL_ADDRESS 			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_VILLAGE_TA 					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_VISIT_DATE 					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_CHEST_INDRAWING 				+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_BREATHS_PER_MINUTE 			+ " INTEGER, "
+									+ TABLE_PATIENT_COLUMN_SLEEPY_UNCONSCIOUS 			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_PALMAR_PALLOR 				+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_MUAC_TAPE_COLOUR 			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_SWELLING_BOTH_FEET 			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_PROBLEM 						+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_COUGH 						+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_COUGH_DURATION 				+ " INTEGER, "
+									+ TABLE_PATIENT_COLUMN_DIARRHOEA 					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_DIARRHOEA_DURATION			+ " INTEGER, "
+									+ TABLE_PATIENT_COLUMN_BLOOD_IN_STOOL				+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_FEVER						+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_FEVER_DURATION				+ " INTEGER, "
+									+ TABLE_PATIENT_COLUMN_CONVULSIONS					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_DIFFICULTY_DRINKING			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_UNABLE_TO_DRINK				+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_VOMITING						+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_VOMITS_EVERYTHING			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_RED_EYE						+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_RED_EYE_DURATION				+ " INTEGER, "
+									+ TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING			+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING_DURATION	+ " INTEGER, "
+									+ TABLE_PATIENT_COLUMN_CANNOT_TREAT					+ " TEXT, "
+									+ TABLE_PATIENT_COLUMN_CANNOT_TREAT_DETAILS			+ " TEXT);";
+
+	public DatabaseHandler(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+	
+	
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(DATABASE_CREATE);
+	}
+
+	
+	@Override
+	public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+		Log.w(DatabaseHandler.class.getName(),
+				"Upgrading database from version " + oldVersion + " to "
+						+ newVersion + ", which will destroy all old data");
+		database.execSQL("DROP TABLE IF EXISTS " + TABLE_PATIENT);
+		onCreate(database);
+	}
+	
+	/**
+	 * Convert a Date instance value to it's  
+	 * string value equivalent
+	 * 
+	 * @param Date
+	 * 
+	 * @return Date
+	 * @throws ParseException 
+	 */
+	public String formatDate(Date date) throws ParseException {
+		if (date != null) {
+			String dateString = new SimpleDateFormat(DateDialogSetListener.DATE_TIME_CUSTOM_FORMAT, DateDialogSetListener.LOCALE)
+										.format(date);
+			return dateString;
+		}
+		else {
+			return null;
+		}
+	}
+
+}
---
+++ SupportingLifeAndroid/src/main/java/ie/ucc/bis/supportinglife/dao/PatientDao.java
@@ -0,0 +1,212 @@
+package ie.ucc.bis.supportinglife.dao;
+
+import ie.ucc.bis.supportinglife.domain.PatientAssessment;
+import ie.ucc.bis.supportinglife.ui.utilities.LoggerUtils;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Class: PatientDao
+ * 
+ * This class maintains the database connection and supports 
+ * adding new patients and fetching all patients.
+ * 
+ * @author TOSullivan
+ */
+public class PatientDao {
+	
+	private final String LOG_TAG = "ie.ucc.bis.supportinglife.dao.PatientDao";
+
+	// Database fields
+	private SQLiteDatabase database;
+	private DatabaseHandler databaseHandler;
+	private String[] allColumns = { DatabaseHandler.TABLE_PATIENT_COLUMN_ID,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_NATIONAL_ID,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_NATIONAL_HEALTH_ID,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_HSA_USER_ID,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_CHILD_FIRST_NAME,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_CHILD_SURNAME,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_BIRTH_DATE,						
+									DatabaseHandler.TABLE_PATIENT_COLUMN_GENDER,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_CAREGIVER_NAME,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_RELATIONSHIP,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_PHYSICAL_ADDRESS,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_VILLAGE_TA,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_VISIT_DATE,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_CHEST_INDRAWING,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_BREATHS_PER_MINUTE,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_SLEEPY_UNCONSCIOUS,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_PALMAR_PALLOR,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_MUAC_TAPE_COLOUR,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_SWELLING_BOTH_FEET,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_PROBLEM,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_COUGH,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_COUGH_DURATION,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_DIARRHOEA,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_DIARRHOEA_DURATION,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_BLOOD_IN_STOOL,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_FEVER,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_FEVER_DURATION,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_CONVULSIONS,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_DIFFICULTY_DRINKING,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_UNABLE_TO_DRINK,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_VOMITING,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_VOMITS_EVERYTHING,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_RED_EYE,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_RED_EYE_DURATION,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING_DURATION,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_CANNOT_TREAT,
+									DatabaseHandler.TABLE_PATIENT_COLUMN_CANNOT_TREAT_DETAILS};
+
+	public PatientDao(Context context) {
+		setDatabaseHandler(new DatabaseHandler(context));
+	}
+
+	public void open() throws SQLException {
+		setDatabase(databaseHandler.getWritableDatabase());
+	}
+
+	public void close() {
+		getDatabaseHandler().close();
+	}
+
+	public PatientAssessment createPatient(PatientAssessment patientToAdd) {
+		ContentValues values = new ContentValues();
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_NATIONAL_ID, patientToAdd.getNationalId());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_NATIONAL_HEALTH_ID, patientToAdd.getNationalHealthId());	
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_HSA_USER_ID, patientToAdd.getHsaUserId());		
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_CHILD_FIRST_NAME, patientToAdd.getChildFirstName());					
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_CHILD_SURNAME, patientToAdd.getChildSurname());						
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_GENDER, patientToAdd.getGender());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_CAREGIVER_NAME, patientToAdd.getCaregiverName());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_RELATIONSHIP, patientToAdd.getRelationship());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_PHYSICAL_ADDRESS, patientToAdd.getPhysicalAddress());	
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_VILLAGE_TA, patientToAdd.getVillageTa());							
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_CHEST_INDRAWING, String.valueOf(patientToAdd.isChestIndrawing()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_BREATHS_PER_MINUTE, patientToAdd.getBreathsPerMinute());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_SLEEPY_UNCONSCIOUS, String.valueOf(patientToAdd.isSleepyUnconscious()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_PALMAR_PALLOR, String.valueOf(patientToAdd.isPalmarPallor()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_MUAC_TAPE_COLOUR, patientToAdd.getMuacTapeColour());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_SWELLING_BOTH_FEET, String.valueOf(patientToAdd.isSwellingBothFeet()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_PROBLEM, patientToAdd.getProblem());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_COUGH, String.valueOf(patientToAdd.isCough()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_COUGH_DURATION, patientToAdd.getCoughDuration());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_DIARRHOEA, String.valueOf(patientToAdd.isDiarrhoea()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_DIARRHOEA_DURATION, patientToAdd.getDiarrhoeaDuration());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_BLOOD_IN_STOOL, String.valueOf(patientToAdd.isBloodInStool()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_FEVER, String.valueOf(patientToAdd.isFever()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_FEVER_DURATION, patientToAdd.getFeverDuration());
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_CONVULSIONS, String.valueOf(patientToAdd.isConvulsions()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_DIFFICULTY_DRINKING, String.valueOf(patientToAdd.isDifficultyDrinkingOrFeeding()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_UNABLE_TO_DRINK, String.valueOf(patientToAdd.isUnableToDrinkOrFeed()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_VOMITING, String.valueOf(patientToAdd.isVomiting()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_VOMITS_EVERYTHING, String.valueOf(patientToAdd.isVomitsEverything()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_RED_EYE, String.valueOf(patientToAdd.isRedEye()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_RED_EYE_DURATION, patientToAdd.getRedEyeDuration());	
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING, String.valueOf(patientToAdd.isDifficultySeeing()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_DIFFICULTY_SEEING_DURATION, patientToAdd.getDifficultySeeingDuration());		
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_CANNOT_TREAT, String.valueOf(patientToAdd.isCannotTreatProblem()));
+		values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_CANNOT_TREAT_DETAILS, patientToAdd.getCannotTreatProblemDetails());	
+	
+		try {
+			values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_BIRTH_DATE, getDatabaseHandler().formatDate(patientToAdd.getBirthDate()));	
+			values.put(DatabaseHandler.TABLE_PATIENT_COLUMN_VISIT_DATE, getDatabaseHandler().formatDate(patientToAdd.getVisitDate()));
+		} catch (ParseException e) {
+			LoggerUtils.i(LOG_TAG, "Parse Exception thrown whilst extracting patient assessment dates");
+			e.printStackTrace();
+		}
+		
+		
+		// add the patient row
+		long insertId = database.insert(DatabaseHandler.TABLE_PATIENT, null, values);
+		
+		Cursor cursor = database.query(DatabaseHandler.TABLE_PATIENT, allColumns, 
+										DatabaseHandler.TABLE_PATIENT_COLUMN_ID + " = " + insertId, 
+										null, null, null, null);
+		cursor.moveToFirst();
+		PatientAssessment patientAssessment = cursorToPatientAssessment(cursor);
+		cursor.close();
+		return patientAssessment;
+	}
+
+
+	public void deletePatient(PatientAssessment patient) {
+		long id = patient.getId();
+		System.out.println("Patient deleted with id: " + id);
+		getDatabase().delete(DatabaseHandler.TABLE_PATIENT, DatabaseHandler.TABLE_PATIENT_COLUMN_ID
+				+ " = " + id, null);
+	}
+
+	
+	public List<PatientAssessment> getAllPatients() {
+		List<PatientAssessment> patients = new ArrayList<PatientAssessment>();
+
+		Cursor cursor = database.query(DatabaseHandler.TABLE_PATIENT,
+				allColumns, null, null, null, null, null);
+
+		cursor.moveToFirst();
+		while (!cursor.isAfterLast()) {
+			PatientAssessment patient = cursorToPatientAssessment(cursor);
+			patients.add(patient);
+			cursor.moveToNext();
+		}
+		// make sure to close the cursor
+		cursor.close();
+		return patients;
+	}
+	
+	private PatientAssessment cursorToPatientAssessment(Cursor cursor) {
+
+//		Param Line 1: 	Integer id, String hsaUserId, String nationalId, String nationalHealthId,
+//		Param Line 2: 	String childFirstName, String childSurname, String birthDate,
+//		Param Line 3: 	String gender, String caregiverName, String relationship, String physicalAddress,
+//		Param Line 4: 	String villageTa, String visitDate, String chestIndrawing, Integer breathsPerMinute,
+//		Param Line 5: 	String sleepyUnconscious, String palmarPallor, String muacTapeColour, 
+//		Param Line 6: 	String swellingBothFeet, String problem, String cough, Integer coughDuration,
+//		Param Line 7: 	String diarrhoea, Integer diarrhoeaDuration, String bloodInStool, String fever,
+//		Param Line 8: 	Integer feverDuration, String convulsions, String difficultyDrinkingOrFeeding,
+//		Param Line 9: 	String unableToDrinkOrFeed, String vomiting, String vomitsEverything,
+//		Param Line 10: 	String redEye, Integer redEyeDuration, String difficultySeeing,
+//		Param Line 11:	Integer difficultySeeingDuration, String cannotTreatProblem, 
+//		Param Line 12: 	String cannotTreatProblemDetails
+		
+		PatientAssessment patientAssessment = new PatientAssessment(cursor.getInt(0), cursor.getString(1), cursor.getString(2), cursor.getString(3),
+													cursor.getString(4), cursor.getString(5), cursor.getString(6),
+													cursor.getString(7), cursor.getString(8), cursor.getString(9), cursor.getString(10),
+													cursor.getString(11), cursor.getString(12), cursor.getString(13), cursor.getInt(14),
+													cursor.getString(15), cursor.getString(16), cursor.getString(17),
+													cursor.getString(18), cursor.getString(19), cursor.getString(20), cursor.getInt(21),
+													cursor.getString(22), cursor.getInt(23), cursor.getString(24), cursor.getString(25),
+													cursor.getInt(26), cursor.getString(27), cursor.getString(28),
+													cursor.getString(29), cursor.getString(30), cursor.getString(31),
+													cursor.getString(32), cursor.getInt(33), cursor.getString(34),
+													cursor.getInt(35), cursor.getString(36),
+													cursor.getString(37));
+		return patientAssessment;
+	}
+
+	public SQLiteDatabase getDatabase() {
+		return database;
+	}
+
+	public void setDatabase(SQLiteDatabase database) {
+		this.database = database;
+	}
+
+	public DatabaseHandler getDatabaseHandler() {
+		return databaseHandler;
+	}
+
+	public void setDatabaseHandler(DatabaseHandler databaseHandler) {
+		this.databaseHandler = databaseHandler;
+	}
+} 
